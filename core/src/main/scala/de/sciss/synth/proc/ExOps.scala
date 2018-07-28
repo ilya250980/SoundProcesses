40e1138b4a7df09a03b89b5b61a95b96d78f9c19
Hanns Holger Rutz <contact@sciss.de>
2018-07-28 21:59:16 +0200

    step

--- core/src/main/scala/de/sciss/synth/proc/ExOps.scala
+++ core/src/main/scala/de/sciss/synth/proc/ExOps.scala
@@ -1,16 +1,29 @@
+/*
+ *  ExOps.scala
+ *  (SoundProcesses)
+ *
+ *  Copyright (c) 2010-2018 Hanns Holger Rutz. All rights reserved.
+ *
+ *	This software is published under the GNU Lesser General Public License v2.1+
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
 package de.sciss.synth.proc
 
-import de.sciss.lucre.expr.ExAttrBridge
+import de.sciss.lucre.expr.ExAttr
 import de.sciss.lucre.expr.impl.{ExAttrBridgeImpl => Impl}
 import de.sciss.synth.Curve
 
 object ExOps {
-  implicit val audioCueExAttrBridge   : ExAttrBridge[AudioCue   ] = new Impl(AudioCue   .Obj)
-  implicit val codeExAttrBridge       : ExAttrBridge[Code       ] = new Impl(Code       .Obj)
-  implicit val colorExAttrBridge      : ExAttrBridge[Color      ] = new Impl(Color      .Obj)
-  implicit val curveExAttrBridge      : ExAttrBridge[Curve      ] = new Impl(CurveObj)
-  implicit val envSegmentExAttrBridge : ExAttrBridge[EnvSegment ] = new Impl(EnvSegment .Obj)
-  implicit val fadeSpecExAttrBridge   : ExAttrBridge[FadeSpec   ] = new Impl(FadeSpec   .Obj)
-//  implicit val fileExAttrBridge       : ExAttrBridge[File       ] = ...
+  implicit val audioCueExAttrBridge   : ExAttr.Bridge[AudioCue   ] = new Impl(AudioCue   .Obj)
+  implicit val codeExAttrBridge       : ExAttr.Bridge[Code       ] = new Impl(Code       .Obj)
+  implicit val colorExAttrBridge      : ExAttr.Bridge[Color      ] = new Impl(Color      .Obj)
+  implicit val curveExAttrBridge      : ExAttr.Bridge[Curve      ] = new Impl(CurveObj)
+  implicit val envSegmentExAttrBridge : ExAttr.Bridge[EnvSegment ] = new Impl(EnvSegment .Obj)
+  implicit val fadeSpecExAttrBridge   : ExAttr.Bridge[FadeSpec   ] = new Impl(FadeSpec   .Obj)
+//  implicit val fileExAttrBridge       : ExAttr.Bridge[File       ] = ...
   //  implicit def ExAttrBridge[Markdown]
 }

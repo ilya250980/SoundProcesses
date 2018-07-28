4abdfc5a5ec259e43ae777c11187ca6684ef90d7
Hanns Holger Rutz <contact@sciss.de>
2018-07-28 02:23:38 +0200

    ex-attr-like sink

--- views/src/main/scala/de/sciss/synth/proc/Widget.scala
+++ views/src/main/scala/de/sciss/synth/proc/Widget.scala
@@ -59,6 +59,7 @@ object Widget extends Obj.Type {
       proc.Code.registerImports(id, Vec(
         "de.sciss.numbers.Implicits._",
         "de.sciss.lucre.expr.ExOps._",
+        "de.sciss.lucre.swing.WidgetOps._",
         "de.sciss.lucre.swing.graph._"
       ))
       proc.Code.registerImports(proc.Code.Action.id, Vec(

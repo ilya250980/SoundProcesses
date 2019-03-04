8bbd35a07bca6ff1a62ba7ce18c429962ea3ad31
Hanns Holger Rutz <contact@sciss.de>
2019-03-04 19:05:19 +0100

    add useful `play` and `playWith` "ops" to graph elements

--- core/src/main/scala/de/sciss/synth/proc/graph/Ops.scala
+++ core/src/main/scala/de/sciss/synth/proc/graph/Ops.scala
@@ -11,9 +11,9 @@
  *  contact@sciss.de
  */
 
-package de.sciss.synth
-package proc
-package graph
+package de.sciss.synth.proc.graph
+
+import de.sciss.synth.GraphFunction
 
 import scala.language.implicitConversions
 
@@ -21,4 +21,12 @@ object Ops {
   /** Allows the construction of attribute controls, for example via `"freq".kr`. */
   implicit def stringToControl(name: String): Attribute.Factory =
     new Attribute.Factory(name)
+
+  /** Allows us to copy ScalaCollider examples into a proc's graph function. */
+  def play[A: GraphFunction.Result](thunk: => A): Unit =
+    playWith()(thunk)
+
+  /** Allows us to copy ScalaCollider examples into a proc's graph function. */
+  def playWith[A](fadeTime: Double = 0.02)(thunk: => A)(implicit result: GraphFunction.Result[A]): Unit =
+    result.close(thunk, fadeTime = fadeTime)
 }

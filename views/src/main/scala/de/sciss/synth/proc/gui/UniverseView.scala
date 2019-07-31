6d0ff3e6b90210373a866e1962d4d51a32043f0a
Hanns Holger Rutz <contact@sciss.de>
2019-07-31 17:31:33 +0200

    remove universe-view (this will have to become a new major version then)

--- views/src/main/scala/de/sciss/synth/proc/gui/UniverseView.scala
+++ views/src/main/scala/de/sciss/synth/proc/gui/UniverseView.scala
@@ -1,25 +1,25 @@
-/*
- *  UniverseView.scala
- *  (SoundProcesses)
- *
- *  Copyright (c) 2010-2019 Hanns Holger Rutz. All rights reserved.
- *
- *	This software is published under the GNU General Public License v2+
- *
- *
- *  For further information, please contact Hanns Holger Rutz at
- *  contact@sciss.de
- */
-
-package de.sciss.synth.proc.gui
-
-import de.sciss.lucre.stm
-import de.sciss.lucre.stm.Sys
-import de.sciss.lucre.swing.View
-import de.sciss.synth.proc.Universe
-
-trait UniverseView[S <: Sys[S]] extends View.Cursor[S] {
-  implicit val universe: Universe[S]
-
-  implicit def cursor: stm.Cursor[S] = universe.cursor
-}
+///*
+// *  UniverseView.scala
+// *  (SoundProcesses)
+// *
+// *  Copyright (c) 2010-2019 Hanns Holger Rutz. All rights reserved.
+// *
+// *	This software is published under the GNU General Public License v2+
+// *
+// *
+// *  For further information, please contact Hanns Holger Rutz at
+// *  contact@sciss.de
+// */
+//
+//package de.sciss.synth.proc.gui
+//
+//import de.sciss.lucre.stm
+//import de.sciss.lucre.stm.Sys
+//import de.sciss.lucre.swing.View
+//import de.sciss.synth.proc.Universe
+//
+//trait UniverseView[S <: Sys[S]] extends View.Cursor[S] {
+//  implicit val universe: Universe[S]
+//
+//  implicit def cursor: stm.Cursor[S] = universe.cursor
+//}

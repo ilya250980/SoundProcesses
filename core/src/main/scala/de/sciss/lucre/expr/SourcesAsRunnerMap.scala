b8478a6b8046ffd552b034b7d0b4b01ec2e60ea3
Hanns Holger Rutz <contact@sciss.de>
2019-08-22 14:24:24 +0200

    allow mixed runner maps of stm.Source and Form

--- core/src/main/scala/de/sciss/lucre/expr/SourcesAsRunnerMap.scala
+++ core/src/main/scala/de/sciss/lucre/expr/SourcesAsRunnerMap.scala
@@ -17,7 +17,14 @@ import de.sciss.lucre.event.Observable
 import de.sciss.lucre.stm
 import de.sciss.lucre.stm.{Form, MapLike, Sys}
 
-final class SourcesAsRunnerMap[S <: Sys[S]](map: Map[String, stm.Source[S#Tx, Form[S]]])
+import SourcesAsRunnerMap.Map
+
+object SourcesAsRunnerMap {
+  type Map[S <: Sys[S]] = scala.collection.immutable.Map[String, Either[stm.Source[S#Tx, Form[S]], Form[S]]]
+}
+/** A runner map with constant keys, and values being either `stm.Obj` sources or forms such as `Const.Expanded`
+  */
+final class SourcesAsRunnerMap[S <: Sys[S]](map: Map[S])
   extends MapLike[S, String, Form] {
 
   def isEmpty (implicit tx: S#Tx): Boolean = map.isEmpty
@@ -29,8 +36,11 @@ final class SourcesAsRunnerMap[S <: Sys[S]](map: Map[String, stm.Source[S#Tx, Fo
   def contains(key: String)(implicit tx: S#Tx): Boolean =
     map.contains(key)
 
-  def get(key: String)(implicit tx: S#Tx): Option[V] =
-    map.get(key).map(_.apply())
+  def get(key: String)(implicit tx: S#Tx): Option[Form[S]] =
+    map.get(key).map {
+      case Left(source) => source()
+      case Right(form)  => form
+    }
 
   def dispose()(implicit tx: S#Tx): Unit = ()
 }

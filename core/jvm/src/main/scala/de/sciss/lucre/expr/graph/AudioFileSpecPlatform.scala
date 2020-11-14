97d66e08dda98e30cb6614f1e784104dc072061d
Hanns Holger Rutz <contact@sciss.de>
2020-11-14 17:13:51 +0100

    audio file spec ex platform ops
    - we really need to find a form of `Ex[Fut[A]]`

--- core/jvm/src/main/scala/de/sciss/lucre/expr/graph/AudioFileSpecPlatform.scala
+++ core/jvm/src/main/scala/de/sciss/lucre/expr/graph/AudioFileSpecPlatform.scala
@@ -1,29 +1,30 @@
 package de.sciss.lucre.expr.graph
 
 import java.io.File
+import java.net.URI
 
+import de.sciss.audiofile.{AudioFile, AudioFileSpec => _AudioFileSpec}
 import de.sciss.lucre.expr.Context
-import de.sciss.lucre.{IExpr, ITargets, Txn}
 import de.sciss.lucre.expr.graph.impl.MappedIExpr
-import de.sciss.audiofile.AudioFile
+import de.sciss.lucre.{IExpr, ITargets, Txn}
 
 import scala.util.Try
-import de.sciss.audiofile.{AudioFileSpec => _AudioFileSpec}
 
 trait AudioFileSpecPlatform {
-  def read(in: Ex[File]): Ex[Option[_AudioFileSpec]] = Read(in)
+  def read(in: Ex[URI]): Ex[Option[_AudioFileSpec]] = Read(in)
 
-  private final class ReadExpanded[T <: Txn[T]](in: IExpr[T, File], tx0: T)
+  private final class ReadExpanded[T <: Txn[T]](in: IExpr[T, URI], tx0: T)
                                                (implicit targets: ITargets[T])
-    extends MappedIExpr[T, File, Option[_AudioFileSpec]](in, tx0) {
+    extends MappedIExpr[T, URI, Option[_AudioFileSpec]](in, tx0) {
 
-    protected def mapValue(inValue: File)(implicit tx: T): Option[_AudioFileSpec] =
-      Try(
-        AudioFile.readSpec(inValue)
-      ).toOption
+    protected def mapValue(inValue: URI)(implicit tx: T): Option[_AudioFileSpec] =
+      Try {
+        val f = new File(inValue)
+        AudioFile.readSpec(f)
+      } .toOption
   }
 
-  final case class Read(in: Ex[File]) extends Ex[Option[_AudioFileSpec]] {
+  final case class Read(in: Ex[URI]) extends Ex[Option[_AudioFileSpec]] {
     override def productPrefix: String = s"AudioFileSpec$$Read" // serialization
 
     type Repr[T <: Txn[T]] = IExpr[T, Option[_AudioFileSpec]]

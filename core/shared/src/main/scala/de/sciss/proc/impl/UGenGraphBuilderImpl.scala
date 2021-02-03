fb4b329dda85a42b9a246e81955bf342a16911d7
Hanns Holger Rutz <contact@sciss.de>
2021-02-03 22:28:54 +0100

    fix inputs/outputs collection in nested ugen graphs (collect at outer) <>

--- core/shared/src/main/scala/de/sciss/proc/impl/UGenGraphBuilderImpl.scala
+++ core/shared/src/main/scala/de/sciss/proc/impl/UGenGraphBuilderImpl.scala
@@ -15,10 +15,10 @@ package de.sciss.proc.impl
 
 import de.sciss.lucre.Txn
 import de.sciss.lucre.synth.Server
-import de.sciss.synth.NestedUGenGraphBuilder.ExpIfCase
-import de.sciss.proc.{Proc, UGenGraphBuilder => UGB}
-import de.sciss.synth.{NestedUGenGraphBuilder, SynthGraph}
 import de.sciss.proc.SoundProcesses.logAural
+import de.sciss.proc.{Proc, UGenGraphBuilder => UGB}
+import de.sciss.synth.NestedUGenGraphBuilder.ExpIfCase
+import de.sciss.synth.{NestedUGenGraphBuilder, SynthGraph}
 
 object UGenGraphBuilderImpl {
   import UGB.{Complete, Context, Incomplete, MissingIn, State}
@@ -64,44 +64,31 @@ object UGenGraphBuilderImpl {
 
   private final class OuterImpl[T <: Txn[T]](protected val context: Context[T],
                                              protected val tx: T)
-    extends NestedUGenGraphBuilder.Outer with Impl[T]
+    extends NestedUGenGraphBuilder.Outer with Impl[T] with UGB.Requester[T] {
 
-  private final class InnerImpl[T <: Txn[T]](protected val childId: Int,
-                                             protected val thisExpIfCase: Option[ExpIfCase],
-                                             protected val parent: NestedUGenGraphBuilder.Basic,
-                                             protected val name: String,
-                                             protected val context: Context[T],
-                                             protected val tx: T)
-    extends NestedUGenGraphBuilder.Inner with Impl[T]
-
-  private trait Impl[T <: Txn[T]]
-    extends NestedUGenGraphBuilder.Basic with UGB with UGB.Requester[T] {
-    builder =>
-
-    // ---- abstract ----
-
-    protected def context: Context[T]
-    protected def tx: T
-
-    // ---- impl ----
-
-    override def toString = s"UGenGraphBuilder.Incomplete@${hashCode.toHexString} (active)"
-
-    protected final def mkInner(childId: Int, thisExpIfCase: Option[ExpIfCase], parent: NestedUGenGraphBuilder.Basic,
-                          name: String): NestedUGenGraphBuilder.Inner =
-      new InnerImpl(childId = childId, thisExpIfCase = thisExpIfCase, parent = parent, name = name,
-                    context = context, tx = tx)
-
-    final var acceptedInputs  = Map.empty[UGB.Key, Map[UGB.Input[_], UGB.Value]]
-    final var outputs         = Map.empty[String, Int]
     private[this] var uniqueId = 0
 
-    final def server: Server = context.server
+    /*override*/ var acceptedInputs = Map.empty[UGB.Key, Map[UGB.Input[_], UGB.Value]]
+    /*override*/ var outputs        = Map.empty[String, Int]
 
-//    final def retry(context: Context[T])(implicit tx: T): State[T] =
-//      throw new IllegalStateException("Cannot retry an ongoing build")
+    override protected def outerImpl: OuterImpl[T] = this
 
-    final def requestInput[Res <: UGB.Value](req: UGB.Input[Res]): Res = {
+    override def allocUniqueId(): Int = {
+      val res = uniqueId
+      uniqueId += 1
+      res
+    }
+
+    def tryBuild(g: SynthGraph): State[T] =
+      try {
+        val result = build(g)
+        new CompleteImpl[T](result, outputs = outputs, acceptedInputs = acceptedInputs)
+      } catch {
+        case MissingIn(rejected) =>
+          new IncompleteImpl[T](g, Set(rejected))
+      }
+
+    override def requestInput[Res <: UGB.Value](req: UGB.Input[Res]): Res = {
       // we pass in `this` and not `in`, because that way the context
       // can find accepted inputs that have been added during the current build cycle!
       val res   = context.requestInput[Res](req, this)(tx)  // IntelliJ highlight bug
@@ -113,30 +100,56 @@ object UGenGraphBuilderImpl {
       res
     }
 
-    final def allocUniqueId(): Int = {
-      val res = uniqueId
-      uniqueId += 1
-      res
-    }
-
-    final def addOutput(key: String, numChannels: Int): Unit =
+    override def addOutput(key: String, numChannels: Int): Unit =
       outputs.get(key).fold {
         outputs += key -> numChannels
-      } { prevChans =>
-        if (numChannels != prevChans) {
+      } { prevChannels =>
+        if (numChannels != prevChannels) {
           val s1 = s"Cannot write multiple times to the same scan ($key)"
-          val s2 = s"using different number of channels ($prevChans, $numChannels)"
+          val s2 = s"using different number of channels ($prevChannels, $numChannels)"
           sys.error(s"$s1 $s2")
         }
       }
+  }
 
-    final def tryBuild(g: SynthGraph): State[T] =
-      try {
-        val result = build(g)
-        new CompleteImpl[T](result, outputs = outputs, acceptedInputs = acceptedInputs)
-      } catch {
-        case MissingIn(rejected) =>
-          new IncompleteImpl[T](g, Set(rejected))
-      }
+  private final class InnerImpl[T <: Txn[T]](protected val childId: Int,
+                                             protected val thisExpIfCase: Option[ExpIfCase],
+                                             protected val outerImpl: OuterImpl[T],
+                                             protected val parent: NestedUGenGraphBuilder.Basic,
+                                             protected val name: String,
+                                             protected val context: Context[T],
+                                             protected val tx: T)
+    extends NestedUGenGraphBuilder.Inner with Impl[T] {
+
+    override def requestInput[Res <: UGB.Value](req: UGB.Input[Res]): Res =
+      outerImpl.requestInput[Res](req)
+
+    override def addOutput(key: String, numChannels: Int): Unit =
+      outerImpl.addOutput(key, numChannels)
+  }
+
+  private trait Impl[T <: Txn[T]]
+    extends NestedUGenGraphBuilder.Basic with UGB /*with UGB.Requester[T]*/ {
+    builder =>
+
+    // ---- abstract ----
+
+    protected def context: Context[T]
+    protected def outerImpl: OuterImpl[T]
+    protected def tx: T
+
+    // ---- impl ----
+
+    override def toString = s"UGenGraphBuilder.Incomplete@${hashCode.toHexString} (active)"
+
+    protected final def mkInner(childId: Int, thisExpIfCase: Option[ExpIfCase], parent: NestedUGenGraphBuilder.Basic,
+                          name: String): NestedUGenGraphBuilder.Inner =
+      new InnerImpl(childId = childId, thisExpIfCase = thisExpIfCase, parent = parent, outerImpl = outerImpl,
+        name = name, context = context, tx = tx)
+
+    final def server: Server = context.server
+
+//    final def retry(context: Context[T])(implicit tx: T): State[T] =
+//      throw new IllegalStateException("Cannot retry an ongoing build")
   }
 }
